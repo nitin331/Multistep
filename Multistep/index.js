333d759f3712903e53f15c61b946a48e4db96cfc
Nitin Jalindar Handal <133699263+nitin331@users.noreply.github.com>
2024-02-13 17:09:46 +0530

    Update index.js

--- Multistep/index.js
+++ Multistep/index.js
@@ -6,43 +6,43 @@ const form3 = document.getElementById("form3");
 const progressEl = document.getElementById("progress");
 const circles = document.querySelectorAll(".circle");
 let currectActive = 1;
-//============== Next Form===============
+
 function nextOne() {
   form1.style.left = "-450px";
   form2.style.left = "25px";
-  //next slide
+
   increamentNumber();
-  // update progress bar
+  
   update();
 }
-//=============== Back One==================
+
 function backOne() {
   form1.style.left = "25px";
   form2.style.left = "450px";
-  // back slide
+  
   decreametNumber();
-  // update progress bar
+  
   update();
 }
-//============ Second Form=============
+
 function nextTwo() {
   form2.style.left = "-450px";
   form3.style.left = "25px";
-  //next slide
+  
   increamentNumber();
-  // update progress bar
+  
   update();
 }
-//=============== Back One==================
+
 function backTwo() {
   form2.style.left = "25px";
   form3.style.left = "450px";
-  // back slide
+  
   decreametNumber();
-  // update progress bar
+  
   update();
 }
-//============= Progress update====================
+
 function update() {
   circles.forEach((circle, indx) => {
     if (indx < currectActive) {
@@ -50,28 +50,28 @@ function update() {
     } else {
       circle.classList.remove("active");
     }
-    // get all of active classes
+    
     const actives = document.querySelectorAll(".active");
     progressEl.style.width =
       ((actives.length - 1) / (circles.length - 1)) * 100 + "%";
   });
 }
-//================== Increament Number===============
+
 function increamentNumber() {
-  // next progress number
+  
   currectActive++;
   if (currectActive > circles.length) {
     currectActive = circles.length;
   }
 }
-//================ Decreament Number=================
+
 function decreametNumber() {
   currectActive--;
   if (currectActive < 1) {
     currectActive = 1;
   }
 }
-//================= btn Events===================
+
 const btnsEvents = () => {
   const next1 = document.getElementById("next1");
   const next2 = document.getElementById("next2");
